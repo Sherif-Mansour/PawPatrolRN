807e19f64fe31d32e7f48adea7aba0a5c2a994fe
Deepinder21 <125156553+Deepinder21@users.noreply.github.com>
2024-05-18 14:10:30 -0600

    Made Sign Up work

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,10 +1,70 @@
 import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
+import ScreenWrapper from '../../components/screenWrapper'
+import { colors } from '../../theme'
 
-export default function SignUpScreen() {
+import BackButton from '../../components/backButton'
+import { useNavigation } from '@react-navigation/native'
+import Snackbar from 'react-native-snackbar';
+
+
+
+export default function SignInScreen() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const navigation = useNavigation();
+
+    const handleSubmit = async ()=>{
+        if(email && password){
+            // good to go
+
+            navigation.navigate('Home');
+            
+        }else{
+            // show error
+            Snackbar.show({
+                text: 'Email and Password are required!',
+                backgroundColor: 'red'
+            });
+        }
+    }
     return (
-        <View>
-            <Text>Hello World</Text>
+        <ScreenWrapper>
+            <View className="flex justify-between h-full mx-4 ">
+                <View>
+                    <View>
+                        <View>
+                            <BackButton />
+                        </View>
+                        <Text>Sign Up</Text>
+                    </View>
+                    <Text>Enter First Name</Text>
+                    <TextInput />
+                    <Text>Enter Last Name</Text>
+                    <TextInput />
+                    <Text>Enter Phone Number</Text>
+                    <TextInput />
+                    <Text>Enter Email</Text>
+                    <TextInput />
+                    <Text>Enter Password</Text>
+                    <TextInput />
+                </View>
+                <View>
+
+                </View>
+                <View>
+            {
+              
+                    <TouchableOpacity onPress={handleSubmit} style={{backgroundColor: colors.button}} className="my-6 rounded-full p-3 shadow-sm mx-2">
+                        <Text className="text-center text-white text-lg font-bold">Sign Up</Text>
+                    </TouchableOpacity>
+                
+            }
+            
         </View>
+            </View>
+        </ScreenWrapper>
     )
 }
