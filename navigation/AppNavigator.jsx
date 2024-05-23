19567b09d730a7c7cd3f47de3808a650eea9a0b7
zacruff <zac.ruff@edu.sait.ca>
2024-05-23 16:09:36 -0600

    Update AppNavigator.jsx
    eslint fixes

--- navigation/AppNavigator.jsx
+++ navigation/AppNavigator.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import SignUpScreen from '../src/screens/_02SignUpScreen';
 import SignInScreen from '../src/screens/_01SignInScreen';
@@ -12,24 +13,50 @@ import PrivacySettingsScreen from '../src/screens/_10PrivacyScreen';
 import SplashScreen from './../src/screens/_00SplashScreen';
 import AppHeader from '../components/Header';
 
-
-
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="SplashScreen">
-                <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
-                <Stack.Screen name="SignIn" component={SignInScreen} options={{ headerShown: false}} />
-                <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false}}/>
-                <Stack.Screen name="Home" component={BottomTabNavigator} options={{ header: () => <AppHeader showBackButton={false} /> }} />
-                <Stack.Screen name="ProfileSettings" component={ProfileSettingsScreen} options={{ header: () => <AppHeader showBackButton={true} /> }} />
-                <Stack.Screen name="NotificationSettings" component={NotificationSettingsScreen} options={{ header: () => <AppHeader showBackButton={true} /> }} />
-                <Stack.Screen name="PrivacySettings" component={PrivacySettingsScreen} options={{ header: () => <AppHeader showBackButton={true} /> }} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen">
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="SignIn"
+          component={SignInScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Home"
+          component={BottomTabNavigator}
+          options={{header: () => <AppHeader showBackButton={false} />}}
+        />
+        <Stack.Screen
+          name="ProfileSettings"
+          component={ProfileSettingsScreen}
+          options={{header: () => <AppHeader showBackButton={true} />}}
+        />
+        <Stack.Screen
+          name="NotificationSettings"
+          component={NotificationSettingsScreen}
+          options={{header: () => <AppHeader showBackButton={true} />}}
+        />
+        <Stack.Screen
+          name="PrivacySettings"
+          component={PrivacySettingsScreen}
+          options={{header: () => <AppHeader showBackButton={true} />}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default AppNavigator;
