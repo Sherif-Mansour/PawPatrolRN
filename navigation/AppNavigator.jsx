c3066f615d0abc1d35ed9e382929910a4279adf5
Deepinder21 <125156553+Deepinder21@users.noreply.github.com>
2024-05-23 16:34:41 -0600

    fixing conflicts

--- navigation/AppNavigator.jsx
+++ navigation/AppNavigator.jsx
@@ -7,12 +7,7 @@ import SignUpScreen from '../src/screens/_02SignUpScreen';
 import LoginScreen from '../src/screens/_01LoginScreen';
 import BottomTabNavigator from './BottomTabNavigator';
 import SettingsScreen from '../src/screens/_07Settings';
-import ProfileSettingsScreen from '../src/screens/_071ProfileScreen';
-import NotificationSettingsScreen from '../src/screens/_072NotificationScreen';
-import PrivacySettingsScreen from '../src/screens/_073PrivacyScreen';
-import PaymentSettingsScreen from '../src/screens/_074PaymentOptionScreen';
-import BookingSettingsScreen from '../src/screens/_075BookingsScreen';
-import AppPreferencesScreen from '../src/screens/_076Apppreference';
+
 
 
 const Stack = createStackNavigator();
@@ -25,12 +20,7 @@ const AppNavigator = () => {
                 <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false}} />
                 <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false}}/>
                 <Stack.Screen name="Home" component={BottomTabNavigator} options={{ headerShown: false}} />
-                <Stack.Screen name="Settings" component={SettingsScreen} options={{ headerShown: false}} />
-                <Stack.Screen name="ProfileSettings" component={ProfileSettingsScreen} />
-                <Stack.Screen name="NotificationSettings" component={NotificationSettingsScreen} />
-                <Stack.Screen name="PrivacySettings" component={PrivacySettingsScreen} />
-                <Stack.Screen name="Payment" component={PaymentSettingsScreen} />
-                <Stack.Screen name="AppPreferences" component={AppPreferencesScreen} />
+                
             </Stack.Navigator>
         </NavigationContainer>
     );
