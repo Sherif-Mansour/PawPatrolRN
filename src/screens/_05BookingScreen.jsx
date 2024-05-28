714756e9a84d7d8d565f35d48ef64161e73e9839
Tao Zhan <130600637+SoulCold404@users.noreply.github.com>
2024-05-28 16:40:13 -0600

    Update _05BookingScreen.jsx

--- src/screens/_05BookingScreen.jsx
+++ src/screens/_05BookingScreen.jsx
@@ -1,32 +1,71 @@
-import {View, Text} from 'react-native';
+
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Button,
+  Dimensions,
+} from 'react-native';
 
 const BookingScreen = () => {
   return (
     <SafeAreaProvider>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderWidth: 1,
-          borderColor: 'black',
-        }}>
-        <Text>This is a placeholder</Text>
-      </View>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderWidth: 1,
-          borderColor: 'black',
-        }}>
-        <Text>This is a placeholder</Text>
-      </View>
+      <ScrollView>
+        <>
+          <View>
+            <Text style={{fontSize: 32, color: 'black'}}> Bookings </Text>
+          </View>
+          <View
+            style={{
+              margin: 16,
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: 370,
+              height: 250,
+              borderColor: 'black',
+              borderRadius: 10,
+              borderWidth: 1,
+            }}>
+            <Text>🐕‍🦺</Text>
+            <Text style={{}}>No Bookings Yet</Text>
+            <Text style={{margin: 10}}>
+              Time to find some lovely services for your pets
+            </Text>
+            <Button title="Start searching" color={'green'} />
+          </View>
+          <View style={{marginTop: 40, marginLeft: 10}}>
+            <Text style={{fontSize: 24, color: 'black'}}>Booking History</Text>
+          </View>
+          <View>
+            <View style={{margin: 10}}>
+              <Text>Service Name: dfdfddfd</Text>
+              <Text>name: John </Text>
+              <Text>Date: </Text>
+            </View>
+
+            <View style={{margin: 10}}>
+              <Text>Service Name: dfdfdfdfd</Text>
+              <Text>name: John </Text>
+              <Text>Date: </Text>
+            </View>
+
+            <View style={{margin: 10}}>
+              <Text>Service Name: dfdfdfdfd</Text>
+              <Text>name: John </Text>
+              <Text>Date: </Text>
+            </View>
+          </View>
+        </>
+      </ScrollView>
+
+  
     </SafeAreaProvider>
   );
 };
 
 export default BookingScreen;
+
