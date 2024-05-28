64af1d9b108ef9c2a17d5e54417cda949e86bdf6
zacruff <zac.ruff@edu.sait.ca>
2024-05-28 14:13:39 -0600

    update

--- src/screens/_03HomeScreen.jsx
+++ src/screens/_03HomeScreen.jsx
@@ -1,13 +1,16 @@
 import { View, Text } from 'react-native';
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AppHeader from '../../components/Header';
+import MySearchBar from '../../components/MySearchBar';
 
 const HomeScreen = () => {
   return (
     <SafeAreaProvider>
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: 'black'}}>
-        <Text>This is a placeholder</Text>
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'flex-start', borderWidth: 1, borderColor: 'black'}}>
+        <View style={{width: '30%'}}>
+          <Text>This is a placeholder</Text>
+        </View>
+          <MySearchBar />
       </View>
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: 'black'}}>
         <Text>This is a placeholder</Text>
