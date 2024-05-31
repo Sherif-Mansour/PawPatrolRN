53b6587169338ed72bec733350bcab90d6dea87e
Sherif-Mansour <135798032+Sherif-Mansour@users.noreply.github.com>
2024-05-31 12:40:18 -0600

    Sherif - Pass navigation to BottomTabNavigation

--- navigation/BottomTabNavigator.jsx
+++ navigation/BottomTabNavigator.jsx
@@ -6,8 +6,12 @@
  * @format
  */
 
-import React, { useState } from 'react';
-import {BottomNavigation, MD3LightTheme as DefaultTheme} from 'react-native-paper';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+  BottomNavigation,
+  MD3LightTheme as DefaultTheme,
+} from 'react-native-paper';
 import HomeScreen from '../src/screens/_03HomeScreen';
 import Favorites from '../src/screens/_04FavoritesScreen';
 import BookingScreen from '../src/screens/_05BookingScreen';
@@ -18,43 +22,62 @@ import customScheme from '../assets/themes/customScheme.json';
 // https://callstack.github.io/react-native-paper/docs/guides/theming
 
 const theme = {
-    ...DefaultTheme,
-    colors: { ...customScheme.colors }
+  ...DefaultTheme,
+  colors: {...customScheme.colors},
 };
 
 // https://youtu.be/Cr5eXyr6CJ4?list=LL "Introduction to React Native and React Native Paper" 36:03
 // https://callstack.github.io/react-native-paper/docs/components/BottomNavigation/ "React Native Paper - Bottom Navigation"
 
 const BottomTabNavigator = () => {
-    const [index, setIndex] = useState(0);
-    const [routes] = useState([
-        { key: 'home', title: 'Home', focusedIcon: 'home-circle', unfocusedIcon: 'home-circle-outline' },
-        { key: 'favorites', title: 'Favorites', focusedIcon: 'heart', unfocusedIcon: 'heart-outline' },
-        { key: 'booking', title: 'Booking', focusedIcon: 'calendar-month' },
-        { key: 'messages', title: 'Messages', focusedIcon: 'chat', unfocusedIcon: 'chat-outline' },
-        { key: 'settings', title: 'Settings', focusedIcon: 'account-circle', unfocusedIcon: 'account-circle-outline' },
-    ]);
+  const navigation = useNavigation();
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {
+      key: 'home',
+      title: 'Home',
+      focusedIcon: 'home-circle',
+      unfocusedIcon: 'home-circle-outline',
+    },
+    {
+      key: 'favorites',
+      title: 'Favorites',
+      focusedIcon: 'heart',
+      unfocusedIcon: 'heart-outline',
+    },
+    {key: 'booking', title: 'Booking', focusedIcon: 'calendar-month'},
+    {
+      key: 'messages',
+      title: 'Messages',
+      focusedIcon: 'chat',
+      unfocusedIcon: 'chat-outline',
+    },
+    {
+      key: 'settings',
+      title: 'Settings',
+      focusedIcon: 'account-circle',
+      unfocusedIcon: 'account-circle-outline',
+    },
+  ]);
 
-    const renderScene = BottomNavigation.SceneMap({
-        home: () => <HomeScreen />,
-        favorites: () => <Favorites />,
-        booking: () => <BookingScreen />,
-        messages: () => <Messages />,
-        settings: () => <Settings />,
-    });
+  const renderScene = BottomNavigation.SceneMap({
+    home: () => <HomeScreen navigation={navigation} />,
+    favorites: () => <Favorites navigation={navigation} />,
+    booking: () => <BookingScreen navigation={navigation} />,
+    messages: () => <Messages navigation={navigation} />,
+    settings: () => <Settings navigation={navigation} />,
+  });
 
-    return (
-        <BottomNavigation
-            navigationState={{ index, routes }}
-            onIndexChange={setIndex}
-            renderScene={renderScene}
-            barStyle={{ backgroundColor: theme.colors.primary }}
-            activeColor={ theme.colors.onPrimary }
-            inactiveColor={ theme.colors.onPrimary }
-        />
-    );
-}
+  return (
+    <BottomNavigation
+      navigationState={{index, routes}}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+      barStyle={{backgroundColor: theme.colors.primary}}
+      activeColor={theme.colors.onPrimary}
+      inactiveColor={theme.colors.onPrimary}
+    />
+  );
+};
 
 export default BottomTabNavigator;
-
-
