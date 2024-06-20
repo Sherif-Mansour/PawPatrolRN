3dd081c246a8562d4bc6c4bf979b633d587939df
Sherif-Mansour <135798032+Sherif-Mansour@users.noreply.github.com>
2024-06-20 12:38:20 -0600

    Sherif - partially implement stripe

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env.local', // Make sure this matches your .env file
+      },
+    ],
+  ],
 };
