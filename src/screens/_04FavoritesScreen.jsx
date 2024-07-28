cb2547ba823a76b176fd7a131cfec8cfe0b1e18d
Tao Zhan <130600637+SoulCold404@users.noreply.github.com>
2024-07-28 15:37:53 -0600

    Update _04FavoritesScreen.jsx

--- src/screens/_04FavoritesScreen.jsx
+++ src/screens/_04FavoritesScreen.jsx
@@ -58,6 +58,7 @@ const FavoritesScreen = ({ navigation }) => {
         renderItem={renderList}
         keyExtractor={item => item.name}
       />
+      <Button>Edit</Button>
     </View>
   );
 };
